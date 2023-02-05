b05c5a5a967b7312de55119438f1d9082848e755
Detlef Groth <detlef.groth@uni-potsdam.de>
2023-02-05 11:18:14 +0100

    fixing space issue after the name

--- bin/rman.R
+++ bin/rman.R
@@ -9,8 +9,9 @@ for (file in args) {
     fin  = file(file, "r")
     flag = FALSE
     while(length((line = readLines(fin,n=1)))>0) {
-        if (grepl("^#' *.+\\\\name\\{.+\\}",line)) {
-            name = gsub("^#' *.+\\\\name\\{(.+)\\}","\\1",line)      
+        if (grepl("^#' *\\\\name\\{.+\\}",line)) {
+                  name = gsub("^#' *\\\\name\\{(.+)\\}.*","\\1",line)      
+                              print(paste("name = '",name,"'",sep=''))
             flag = TRUE
             mandir = sub("R/.+","man/",file)
             fout = file(paste(mandir,name,".Rd",sep=""),"w")
