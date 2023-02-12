f8f899f68a5dabad89659a72c1500a57eaa682cd
Detlef Groth <detlef.groth@uni-potsdam.de>
2023-02-12 21:05:34 +0100

    hanna-package.R file added

---
+++ R/hanna-package.R
@@ -0,0 +1,19 @@
+#' \docType{package}
+#' \name{hanna-package}
+#' \alias{hanna}
+#' \alias{hanna-package}
+#' \title{Package hanna - simulations for winner-looser effects}
+#' \description{This package contains the code for the paper 
+#'   "Hermanussen et. al. (2023): Winner-loser effects improve social network efficiency among 
+#'    competitors with equal resource holding power."
+#' 
+#'  The following objects / modules are available:
+#' \itemize{
+#' 
+#' \item{\link[hanna:hgraph]{hgraph}}{ R utility code functions for working with graphs}
+#' \item{\link[hanna:simul]{simul}}{ R code for the simulation of the effect}
+#' }}
+#'
+#' \author{Detlef Groth, University of Potsdam, Germany}
+#' 
+""
