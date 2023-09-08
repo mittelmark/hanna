ffd899e93277d18cb8160d8ce95325856d7f3035
Detlef Groth <detlef.groth@uni-potsdam.de>
2023-09-08 15:57:37 +0200

    adding benchmarking script with first code

--- bin/benchmark.R
+++ bin/benchmark.R
@@ -1,3 +1,13 @@
+source("R/simul.R")
+source("R/hgraph.R")
 t1=Sys.time()
-Sys.sleep(1.5)
+mode="random"
+for (i in 1:10) {
+    if (i == 1) {
+        res=simul$season(LETTERS[1:20],model=mode)
+    } else {
+        res=simul$season(LETTERS[1:20],token=res$token,model=mode)
+    }  
+}
+print(unlist(hgraph$triads(simul$graph(res$M,mode="win"))))
 print(Sys.time()-t1)
